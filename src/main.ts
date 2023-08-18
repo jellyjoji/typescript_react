9ea20461802108d608331ad956d1cfe4a68eed1b
서재원 <cinos81@nhnlink.co.kr>
2023-08-18 16:48:13 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <div>hello</div>
   </React.StrictMode>,
 )
